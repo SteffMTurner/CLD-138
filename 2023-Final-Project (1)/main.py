d37c140ab07b345ce5b76332d1d821d17727202c
SteffMTurner <146963612+SteffMTurner@users.noreply.github.com>
2023-10-13 12:55:14 -0500

    main.py

--- 2023-Final-Project (1)/main.py
+++ 2023-Final-Project (1)/main.py
@@ -4,13 +4,18 @@ from Player import Player
 from WarGame import WarCardGame
 
 player = Player("You", Deck(is_empty=True))
-computer = Player("Computer", Deck(is_empty=True), is_Computer=True)
-deck = Deck(is_empty=True)
+computer = Player("Computer", Deck(is_empty=True), is_computer=True)
+deck = Deck()
+
 game = WarCardGame(player, computer, deck)
-game.print_welcome()
+
+game.print_welcome_message()
+
 while not game.check_game_over():
-  game.start_battle()
-  game.print_stats()
-  answer = input("\n Ready for next round? \nPress Enter to continue, or x to stop!")
-  if answer.lower() == "x":
-    break
+    game.start_battle()
+    game.print_stats()
+
+    answer = input("\nAre you ready for the next round?\nPress Enter to continue. Enter X to stop.")
+
+    if answer.lower() == "x":
+        break
