5448c4321342b2fbc28fa6c39dd153dd2eb53490
SteffMTurner <146963612+SteffMTurner@users.noreply.github.com>
2023-10-13 12:49:56 -0500

    Player.py

--- 2023-Final-Project (1)/Player.py
+++ 2023-Final-Project (1)/Player.py
@@ -1,22 +1,26 @@
 class Player:
-  def __init__ (self, name, deck, is_Computer=False):
-    self._name = name
-    self._deck = deck
-    self._is_Computer = is_Computer
 
-    @property
-    def is_Computer(self):
-      return self._is_Computer
-    @property
-    def deck(self):
-      return self.deck
-  
+  def __init__(self, name, deck, is_computer=False):
+    self.name = name
+    self._deck = deck
+    self._is_computer = is_computer
+
+  @property
+  def is_computer(self):
+    return self._is_computer
+
+  @property
+  def deck(self):
+    return self._deck
+
   def has_empty_deck(self):
-      return self._deck.size == 0
+    return self._deck.size == 0
+
   def draw_card(self):
-    if self.has_empty_deck():
-      return self._deck.draw()
+    if not self.has_empty_deck():
+        return self._deck.draw()
     else:
-      return None
+        return None
+
   def add_card(self, card):
     self._deck.add(card)
